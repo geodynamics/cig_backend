2d1c42d8ffab4bdb686f04e0e19f4bdc4c89ba81
Eric Heien <emheien@geoshell.geodynamics.org>
2013-04-17 12:56:36 -0700

    Merged scripts to a single queue daemon

--- queue/queue_daemon.sh
+++ queue/queue_daemon.sh
@@ -3,30 +3,47 @@
 # Fail out if there are any errors
 set -e
 
-if [ $# -ne 1 ]
+if [ $# -ne 1 ] && [ $# -ne 2 ]
 then
-	echo "Usage: $0 <queue location>"
+	echo "Usage: $0 <queue name> <optional command>"
 	exit 1
 fi
 
-QUEUE_LOC="$1"
-LOCK_FILE=$QUEUE_LOC/lockfile
+# Ignore SIGHUP to allow running in the background even after logging out
+trap "" 1
 
-if [ ! -d $QUEUE_LOC ];
+# Get the queue directory
+QUEUE_LOC="`pwd`/queues/$1"
+mkdir -p $QUEUE_LOC
+
+# If a command was specified, add it to the queue
+if [ $# -eq 2 ]
 then
-    echo "ERROR: $QUEUE_LOC does not exist."
-    exit 1
+    CMD="$2"
+    QUEUE_ENTRY="`date +%s`_$$"
+    echo "$CMD" > $QUEUE_LOC/$QUEUE_ENTRY
 fi
 
-exec 9>$LOCK_FILE
+# Start the daemon if there isn't one already running
+QUEUE_LOCK_FILE=$QUEUE_LOC/queue.lockfile
+QUEUE_LOG_FILE=$QUEUE_LOC/queue.log
+
+exec 9>$QUEUE_LOCK_FILE
 if ! flock -n 9 ; then
-    echo "Daemon for $QUEUE_LOC is already running"
-    exit 1
+    exit 0
 fi
 
-while true;
-do
-    NEXT_FILE=`ls $QUEUE_LOC/*_* | sort -n | head -n 1`
-    echo $NEXT_FILE
+# Enter an infinite loop to look for new queue entries,
+# execute them, and write the results to the log
+while true; do
+    NEXT_FILE=`ls $QUEUE_LOC/*_* 2>/dev/null | sort -n | head -n 1 2>/dev/null` || true
+    if [ "$NEXT_FILE" != "" ];
+    then
+        echo "Running $NEXT_FILE" >> $QUEUE_LOG_FILE
+        ( source $NEXT_FILE >>$QUEUE_LOG_FILE 2>&1 ) || true
+        rm $NEXT_FILE
+    else
+        sleep 5
+    fi
 done
 
