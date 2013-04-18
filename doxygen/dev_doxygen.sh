71509cde5df5dff216549347a0d71919b7988ea7
docs coding testing <backend@geoshell.geodynamics.org>
2013-04-18 10:37:37 -0700

    Added queueing to doxygen generation

--- doxygen/dev_doxygen.sh
+++ doxygen/dev_doxygen.sh
@@ -1,42 +1,43 @@
 #!/bin/sh
 
 MAX_SVN_REV=`svn info http://geodynamics.org/svn/cig | grep Revision | awk '{print $2}'`
+QUEUE_CMD="../queue/queue_daemon.sh doxy_queue"
 
 ###############################
 # Short-Term Crustal Dynamics #
 ###############################
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/short/3D/PyLith/trunk/ $MAX_SVN_REV PyLith
-./generate_doxygen.sh hg http://geodynamics.org/hg/short/3D/relax 195 RELAX
-./generate_doxygen.sh git https://github.com/eheien/selen.git a0bdb9ee732d093d85c5f601a11101013f5dbc47 SELEN
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/short/3D/PyLith/trunk/ $MAX_SVN_REV PyLith" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh hg http://geodynamics.org/hg/short/3D/relax 195 RELAX" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh git https://github.com/eheien/selen.git a0bdb9ee732d093d85c5f601a11101013f5dbc47 SELEN" &
 
 #######################
 # Long-Term Tectonics #
 #######################
-./generate_doxygen.sh hg http://geodynamics.org/hg/long/3D/gale 475 Gale
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/long/2D/plasti/trunk $MAX_SVN_REV Plasti
+#$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh hg http://geodynamics.org/hg/long/3D/gale 475 Gale" &
+#$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/long/2D/plasti/trunk $MAX_SVN_REV Plasti" &
 
 #####################
 # Mantle Convection #
 #####################
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/mc/3D/CitcomCU/trunk $MAX_SVN_REV CitcomCU
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/mc/3D/CitcomS/trunk $MAX_SVN_REV CitcomS
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/mc/2D/ConMan/trunk $MAX_SVN_REV ConMan
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/mc/1D/hc/trunk $MAX_SVN_REV HC
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/mc/3D/CitcomCU/trunk $MAX_SVN_REV CitcomCU" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/mc/3D/CitcomS/trunk $MAX_SVN_REV CitcomS" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/mc/2D/ConMan/trunk $MAX_SVN_REV ConMan" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/mc/1D/hc/trunk $MAX_SVN_REV HC" &
 
 ##############
 # Seismology #
 ##############
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/3D/SPECFEM3D/trunk $MAX_SVN_REV SPECFEM3D_Cartesian
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/3D/SPECFEM3D_GLOBE/trunk/ $MAX_SVN_REV SPECFEM3D_GLOBE
-#./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/3D/SPECFEM3D_GEOTECH/trunk/ $MAX_SVN_REV SPECFEM3D_GEOTECH
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/2D/SPECFEM2D/trunk $MAX_SVN_REV SPECFEM2D
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/1D/SPECFEM1D/trunk $MAX_SVN_REV SPECFEM1D
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/1D/mineos/trunk $MAX_SVN_REV Mineos
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/3D/ADJOINT_TOMO/flexwin/trunk $MAX_SVN_REV Flexwin
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/3D/CPML/trunk $MAX_SVN_REV SEISMIC_CPML
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/3D/SPECFEM3D/trunk $MAX_SVN_REV SPECFEM3D_Cartesian" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/3D/SPECFEM3D_GLOBE/trunk/ $MAX_SVN_REV SPECFEM3D_GLOBE" &
+#$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/3D/SPECFEM3D_GEOTECH/trunk/ $MAX_SVN_REV SPECFEM3D_GEOTECH" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/2D/SPECFEM2D/trunk $MAX_SVN_REV SPECFEM2D" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/1D/SPECFEM1D/trunk $MAX_SVN_REV SPECFEM1D" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/1D/mineos/trunk $MAX_SVN_REV Mineos" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/3D/ADJOINT_TOMO/flexwin/trunk $MAX_SVN_REV Flexwin" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/seismo/3D/CPML/trunk $MAX_SVN_REV SEISMIC_CPML" &
 
 #############
 # Geodynamo #
 #############
-./generate_doxygen.sh svn http://geodynamics.org/svn/cig/geodyn/3D/MAG/trunk $MAX_SVN_REV MAG
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh svn http://geodynamics.org/svn/cig/geodyn/3D/MAG/trunk $MAX_SVN_REV MAG" &
 
--- doxygen/release_doxygen.sh
+++ doxygen/release_doxygen.sh
@@ -1,43 +1,45 @@
 #!/bin/sh
 
+QUEUE_CMD="../queue/queue_daemon.sh doxy_queue"
+
 ###############################
 # Short-Term Crustal Dynamics #
 ###############################
-./generate_doxygen.sh url http://geodynamics.org/cig/software/pylith/pylith-1.8.0.tgz 1.8.0 PyLith
-./generate_doxygen.sh url http://geodynamics.org/cig/software/relax/Relax-1_0_4.tgz 1.0.4 RELAX
-./generate_doxygen.sh url http://geodynamics.org/cig/software/selen/SELEN_2.9.10.4.tar.gz 2.9.10.4 SELEN
-./generate_doxygen.sh url http://geodynamics.org/cig/software/lithomop/lithomop3d-0.7.2.tar.gz 0.7.2 LithoMop
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/pylith/pylith-1.8.0.tgz 1.8.0 PyLith" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/relax/Relax-1_0_4.tgz 1.0.4 RELAX" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/selen/SELEN_2.9.10.4.tar.gz 2.9.10.4 SELEN" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/lithomop/lithomop3d-0.7.2.tar.gz 0.7.2 LithoMop" &
 
 #######################
 # Long-Term Tectonics #
 #######################
-./generate_doxygen.sh url http://geodynamics.org/cig/software/gale/Gale-2_0_1.tgz 2.0.1 Gale
-./generate_doxygen.sh url http://geodynamics.org/cig/software/plasti/plasti-1.0.0.tar.gz 1.0.0 Plasti
-./generate_doxygen.sh url http://geodynamics.org/cig/software/snac/SNAC-1.2.0.tar.gz 1.2.0 SNAC
+#$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/gale/Gale-2_0_1.tgz 2.0.1 Gale" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/plasti/plasti-1.0.0.tar.gz 1.0.0 Plasti" &
+#$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/snac/SNAC-1.2.0.tar.gz 1.2.0 SNAC" &
 
 #####################
 # Mantle Convection #
 #####################
-./generate_doxygen.sh url http://geodynamics.org/cig/software/citcomcu/CitcomCU-1.0.3.tar.gz 1.0.3 CitcomCU
-./generate_doxygen.sh url http://geodynamics.org/cig/software/citcoms/CitcomS-3.2.0.tar.gz 3.2.0 CitcomS
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/citcomcu/CitcomCU-1.0.3.tar.gz 1.0.3 CitcomCU" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/citcoms/CitcomS-3.2.0.tar.gz 3.2.0 CitcomS" &
 # ConMan distribution isn't packaged correctly
 #./generate_doxygen.sh url http://geodynamics.org/cig/software/conman/ConMan-2.0.0.tar.gz 2.0.0 ConMan
-./generate_doxygen.sh url http://geodynamics.org/cig/software/hc/HC-1_0.tgz 1.0 HC
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/hc/HC-1_0.tgz 1.0 HC" &
 
 ##############
 # Seismology #
 ##############
-./generate_doxygen.sh url http://geodynamics.org/cig/software/specfem3d/SPECFEM3D_Cartesian_V2.0.2.tar.gz 2.0.2 SPECFEM3D_Cartesian
-./generate_doxygen.sh url http://geodynamics.org/cig/software/specfem3d-globe/SPECFEM3D_GLOBE_V5.1.5.tar.gz 5.1.5 SPECFEM3D_GLOBE
-./generate_doxygen.sh url http://geodynamics.org/cig/software/specfem3d-geotech/SPECFEM3D_GEOTECH_V1.1b.tar.gz 1.1b SPECFEM3D_GEOTECH
-./generate_doxygen.sh url http://geodynamics.org/cig/software/specfem2d/SPECFEM2D-7.0.0.tar.gz 7.0.0 SPECFEM2D
-./generate_doxygen.sh url http://geodynamics.org/cig/software/specfem1d/SPECFEM1D-1.0.3.tar.gz 1.0.3 SPECFEM1D
-./generate_doxygen.sh url http://geodynamics.org/cig/software/mineos/mineos-1.0.2.tgz 1.0.2 Mineos
-./generate_doxygen.sh url http://geodynamics.org/cig/software/flexwin/FLEXWIN-1.0.1.tar.gz 1.0.1 Flexwin
-./generate_doxygen.sh url http://geodynamics.org/cig/software/seismic_cpml/SEISMIC_CPML_1.2.tar.gz 1.2 SEISMIC_CPML
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/specfem3d/SPECFEM3D_Cartesian_V2.0.2.tar.gz 2.0.2 SPECFEM3D_Cartesian" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/specfem3d-globe/SPECFEM3D_GLOBE_V5.1.5.tar.gz 5.1.5 SPECFEM3D_GLOBE" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/specfem3d-geotech/SPECFEM3D_GEOTECH_V1.1b.tar.gz 1.1b SPECFEM3D_GEOTECH" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/specfem2d/SPECFEM2D-7.0.0.tar.gz 7.0.0 SPECFEM2D" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/specfem1d/SPECFEM1D-1.0.3.tar.gz 1.0.3 SPECFEM1D" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/mineos/mineos-1.0.2.tgz 1.0.2 Mineos" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/flexwin/FLEXWIN-1.0.1.tar.gz 1.0.1 Flexwin" &
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/seismic_cpml/SEISMIC_CPML_1.2.tar.gz 1.2 SEISMIC_CPML" &
 
 #############
 # Geodynamo #
 #############
-./generate_doxygen.sh url http://geodynamics.org/cig/software/mag/MAG-1.0.2.tar.gz 1.0.2 MAG
+$QUEUE_CMD "cd `pwd` ; ./generate_doxygen.sh url http://geodynamics.org/cig/software/mag/MAG-1.0.2.tar.gz 1.0.2 MAG" &
 
