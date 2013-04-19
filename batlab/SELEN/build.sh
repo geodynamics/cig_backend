8f8b11850b838d6f5e6537f56f3f38993f4c182e
Eric Heien <github@heien.org>
2013-04-19 12:15:19 -0700

    Added file for fast build/run testing and distribution testing of SELEN

--- batlab/SELEN/build.sh
+++ batlab/SELEN/build.sh
@@ -2,8 +2,10 @@
 #
 # $Id: build.sh 21340 2013-02-07 07:36:36Z ericheien $
 
+# To handle different names for distribution vs. repo
+mv SELEN* selen
 # Build SELEN
-source ./build_common.sh && cd selen && ./configure && make
+source ./build_common.sh && mv fast_config.dat selen/config.dat && cd selen && ./configure && make run
 
 exit $?
 
