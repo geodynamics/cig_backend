f8277ad602a6d7656875c47d1a4baf19261c948f
Eric Heien <github@heien.org>
2013-04-19 15:48:45 -0500

    Updated download script

--- batlab/setup_debug.sh
+++ batlab/setup_debug.sh
@@ -10,21 +10,22 @@ wget http://www.geodynamics.org/cig/software/citcoms/CitcomS-3.2.0.tar.gz
 wget http://www.geodynamics.org/cig/software/pylith/pylith-1.8.0.tgz
 wget http://www.geodynamics.org/cig/software/mag/MAG-1.0.2.tar.gz
 wget http://www.geodynamics.org/cig/software/mineos/mineos-1.0.2.tgz
+wget http://www.geodynamics.org/cig/software/selen/SELEN_2.9.10.4.tar.gz
 cd ..
 
 # Get support files/libraries needed by CIG codes
 cd support
-rm deal.nodoc-7.2.0.tar.gz ; wget https://dealii.googlecode.com/files/deal.nodoc-7.2.0.tar.gz
-rm fftw-3.3.3.tar.gz ; wget http://www.fftw.org/fftw-3.3.3.tar.gz
-rm gmt-4.5.8.tar.bz2 ; wget ftp://ftp.iris.washington.edu/pub/gmt/gmt-4.5.8.tar.bz2
-rm nemesis-1.0.2.tar.gz ; wget http://www.geodynamics.org/cig/software/pythia/nemesis-1.0.2.tar.gz
-rm netcdf.tar.gz ; wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf.tar.gz
-rm numdiff-5.6.1.tar.gz ; wget http://gnu.mirrors.pair.com/savannah/savannah/numdiff/numdiff-5.6.1.tar.gz
-rm numpy-1.7.0rc1.tar.gz ; wget http://downloads.sourceforge.net/project/numpy/NumPy/1.7.0rc1/numpy-1.7.0rc1.tar.gz
-rm openmpi-1.6.3.tar.bz2 ; wget http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.3.tar.bz2
-rm petsc-dev-pylith-1.8.0.tgz ; wget http://www.geodynamics.org/cig/software/pylith/petsc-dev-pylith-1.8.0.tgz
-rm proj-4.8.0.tar.gz ; wget http://download.osgeo.org/proj/proj-4.8.0.tar.gz
-rm spatialdata-1.9.0.tgz ; wget http://www.geodynamics.org/cig/software/pylith/spatialdata-1.9.0.tgz
-rm trilinos-11.0.3-Source.tar.bz2 ; wget http://trilinos.sandia.gov/download/files/trilinos-11.0.3-Source.tar.bz2
+rm -f deal.offlinedoc-7.3.0.tar.gz ; wget https://dealii.googlecode.com/files/deal.offlinedoc-7.3.0.tar.gz
+rm -f fftw-3.3.3.tar.gz ; wget http://www.fftw.org/fftw-3.3.3.tar.gz
+rm -f gmt-4.5.8.tar.bz2 ; wget ftp://ftp.iris.washington.edu/pub/gmt/gmt-4.5.8.tar.bz2
+rm -f nemesis-1.0.2.tar.gz ; wget http://www.geodynamics.org/cig/software/pythia/nemesis-1.0.2.tar.gz
+rm -f netcdf.tar.gz ; wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf.tar.gz
+rm -f numdiff-5.6.1.tar.gz ; wget http://gnu.mirrors.pair.com/savannah/savannah/numdiff/numdiff-5.6.1.tar.gz
+rm -f numpy-1.7.0rc1.tar.gz ; wget http://downloads.sourceforge.net/project/numpy/NumPy/1.7.0rc1/numpy-1.7.0rc1.tar.gz
+rm -f openmpi-1.6.3.tar.bz2 ; wget http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.3.tar.bz2
+rm -f petsc-dev-pylith-1.8.0.tgz ; wget http://www.geodynamics.org/cig/software/pylith/petsc-dev-pylith-1.8.0.tgz
+rm -f proj-4.8.0.tar.gz ; wget http://download.osgeo.org/proj/proj-4.8.0.tar.gz
+rm -f spatialdata-1.9.0.tgz ; wget http://www.geodynamics.org/cig/software/pylith/spatialdata-1.9.0.tgz
+rm -f trilinos-11.0.3-Source.tar.bz2 ; wget http://trilinos.sandia.gov/download/files/trilinos-11.0.3-Source.tar.bz2
 cd ..
 
