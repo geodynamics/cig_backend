40c8f609da30b47e71e5a6e60d3a5becc281353c
Eric Heien <github@heien.org>
2013-05-01 15:06:55 -0700

    Add repository type to code DB

--- cig_codes.py
+++ cig_codes.py
@@ -3,13 +3,15 @@
 class CodeDB:
     def __init__(self):
         self.repo_url = {}
+        self.repo_type = {}
         self.release_src = {}
         self.release_version = {}
         self.dev_doxygen = {}
         self.release_doxygen = {}
 
-    def register(self, name, repo_url, release_src, release_version, dev_doxygen, release_doxygen):
+    def register(self, name, repo_url, repo_type, release_src, release_version, dev_doxygen, release_doxygen):
         self.repo_url[name] = repo_url
+        self.repo_type[name] = repo_type
         self.release_src[name] = release_src
         self.release_version[name] = release_version
         self.dev_doxygen[name] = dev_doxygen
@@ -31,6 +33,7 @@ code_db = CodeDB()
 ###############################
 code_db.register(name="PyLith",
                  repo_url="short/3D/PyLith/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/pylith/pylith-1.8.0.tgz",
                  release_version="1.8.0",
                  dev_doxygen=True,
@@ -38,6 +41,7 @@ code_db.register(name="PyLith",
 
 code_db.register(name="RELAX",
                  repo_url="",
+                 repo_type="none",
                  release_src="http://geodynamics.org/cig/software/relax/Relax-1_0_4.tgz",
                  release_version="1.0.4",
                  dev_doxygen=True,
@@ -45,6 +49,7 @@ code_db.register(name="RELAX",
 
 code_db.register(name="SELEN",
                  repo_url="github.com/eheien/selen",
+                 repo_type="git",
                  release_src="http://geodynamics.org/cig/software/selen/SELEN_2.9.10.4.tar.gz",
                  release_version="2.9.10.4",
                  dev_doxygen=True,
@@ -52,6 +57,7 @@ code_db.register(name="SELEN",
 
 code_db.register(name="LithoMop",
                  repo_url="",
+                 repo_type="none",
                  release_src="http://geodynamics.org/cig/software/lithomop/lithomop3d-0.7.2.tar.gz",
                  release_version="0.7.2",
                  dev_doxygen=True,
@@ -62,6 +68,7 @@ code_db.register(name="LithoMop",
 #######################
 code_db.register(name="Gale",
                  repo_url="long/3D/gale/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/gale/Gale-2_0_1.tgz",
                  release_version="2.0.1",
                  dev_doxygen=False,
@@ -69,6 +76,7 @@ code_db.register(name="Gale",
 
 code_db.register(name="Plasti",
                  repo_url="long/2D/plasti/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/plasti/plasti-1.0.0.tar.gz",
                  release_version="1.0.0",
                  dev_doxygen=True,
@@ -76,6 +84,7 @@ code_db.register(name="Plasti",
 
 code_db.register(name="SNAC",
                  repo_url="long/3D/SNAC/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/snac/SNAC-1.2.0.tar.gz",
                  release_version="1.2.0",
                  dev_doxygen=False,
@@ -86,6 +95,7 @@ code_db.register(name="SNAC",
 #####################
 code_db.register(name="CitcomCU",
                  repo_url="mc/3D/CitcomCU/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/citcomcu/CitcomCU-1.0.3.tar.gz",
                  release_version="1.0.3",
                  dev_doxygen=True,
@@ -93,6 +103,7 @@ code_db.register(name="CitcomCU",
 
 code_db.register(name="CitcomS",
                  repo_url="mc/3D/CitcomS/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/citcoms/CitcomS-3.2.0.tar.gz",
                  release_version="3.2.0",
                  dev_doxygen=True,
@@ -100,6 +111,7 @@ code_db.register(name="CitcomS",
 
 code_db.register(name="ConMan",
                  repo_url="mc/2D/ConMan/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/conman/ConMan-2.0.0.tar.gz",
                  release_version="2.0.0",
                  dev_doxygen=False,
@@ -107,6 +119,7 @@ code_db.register(name="ConMan",
 
 code_db.register(name="Ellipsis3D",
                  repo_url="mc/3D/ellipsis3d/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/ellipsis3d/Ellipsis3D-1.0.2.tar.gz",
                  release_version="1.0.2",
                  dev_doxygen=False,
@@ -114,6 +127,7 @@ code_db.register(name="Ellipsis3D",
 
 code_db.register(name="HC",
                  repo_url="mc/1D/hc/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/hc/HC-1_0.tgz",
                  release_version="1.0",
                  dev_doxygen=True,
@@ -124,6 +138,7 @@ code_db.register(name="HC",
 ##############
 code_db.register(name="SPECFEM3D_Cartesian",
                  repo_url="seismo/3D/SPECFEM3D/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/specfem3d/SPECFEM3D_Cartesian_V2.0.2.tar.gz",
                  release_version="2.0.2",
                  dev_doxygen=True,
@@ -131,6 +146,7 @@ code_db.register(name="SPECFEM3D_Cartesian",
 
 code_db.register(name="SPECFEM3D_GLOBE",
                  repo_url="seismo/3D/SPECFEM3D_GLOBE/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/specfem3d-globe/SPECFEM3D_GLOBE_V5.1.5.tar.gz",
                  release_version="5.1.5",
                  dev_doxygen=True,
@@ -138,6 +154,7 @@ code_db.register(name="SPECFEM3D_GLOBE",
 
 code_db.register(name="SPECFEM3D_GEOTECH",
                  repo_url="seismo/3D/SPECFEM3D_GEOTECH/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/specfem3d-geotech/SPECFEM3D_GEOTECH_V1.1b.tar.gz",
                  release_version="1.1b",
                  dev_doxygen=False,
@@ -145,6 +162,7 @@ code_db.register(name="SPECFEM3D_GEOTECH",
 
 code_db.register(name="SPECFEM2D",
                  repo_url="seismo/2D/SPECFEM2D/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/specfem2d/SPECFEM2D-7.0.0.tar.gz",
                  release_version="7.0.0",
                  dev_doxygen=True,
@@ -152,6 +170,7 @@ code_db.register(name="SPECFEM2D",
 
 code_db.register(name="SPECFEM1D",
                  repo_url="seismo/1D/SPECFEM1D/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/specfem1d/SPECFEM1D-1.0.3.tar.gz",
                  release_version="1.0.3",
                  dev_doxygen=True,
@@ -159,6 +178,7 @@ code_db.register(name="SPECFEM1D",
 
 code_db.register(name="Mineos",
                  repo_url="seismo/1D/mineos/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/mineos/mineos-1.0.2.tgz",
                  release_version="1.0.2",
                  dev_doxygen=True,
@@ -166,6 +186,7 @@ code_db.register(name="Mineos",
 
 code_db.register(name="Flexwin",
                  repo_url="seismo/3D/ADJOINT_TOMO/flexwin",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/flexwin/FLEXWIN-1.0.1.tar.gz",
                  release_version="1.0.1",
                  dev_doxygen=True,
@@ -173,6 +194,7 @@ code_db.register(name="Flexwin",
 
 code_db.register(name="SEISMIC_CPML",
                  repo_url="seismo/3D/CPML/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/seismic_cpml/SEISMIC_CPML_1.2.tar.gz",
                  release_version="1.2",
                  dev_doxygen=True,
@@ -183,6 +205,7 @@ code_db.register(name="SEISMIC_CPML",
 #############
 code_db.register(name="MAG",
                  repo_url="geodyn/3D/MAG/trunk",
+                 repo_type="svn",
                  release_src="http://geodynamics.org/cig/software/mag/MAG-1.0.2.tar.gz",
                  release_version="1.0.2",
                  dev_doxygen=True,
