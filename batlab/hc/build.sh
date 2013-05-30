37724a941cf9bb50c9c31c184b1a468830c575d0
Eric Heien <github@heien.org>
2013-05-30 15:48:18 -0500

    Fixed HC build script

--- batlab/hc/build.sh
+++ batlab/hc/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+source ./build_common.sh
+
 # To handle different names for distribution vs. repo
 cd HC* || cd hc*
 
 # Build HC
-source ./build_common.sh && export GMTHOME=$HOME/local/ && export NETCDFHOME=$HOME/local/ && make run
+export GMTHOME=$HOME/local/ && export NETCDFHOME=$HOME/local/ && make
 
 exit $?
 
