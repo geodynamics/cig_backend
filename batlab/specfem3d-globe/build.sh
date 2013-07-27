816dfe085b92f2874513c915acef70d5b09de61a
Eric Heien <github@heien.org>
2013-07-27 10:38:09 -0500

    Update SPECFEM3D and globe tests

--- batlab/specfem3d-globe/build.sh
+++ batlab/specfem3d-globe/build.sh
@@ -3,9 +3,11 @@
 # Point to any additional installed libraries
 source ./build_common.sh
 
+# Ensure we have a common naming scheme
+mv SPECFEM3D*/ specfem3d_globe > /dev/null 2>&1
+
 # Build SPECFEM3D Globe
-cd SPECFEM3D* || cd specfem3d*
-./configure && make all
+cd specfem3d_globe && ./configure && make all && cd .. && tar -czf results.tar.gz specfem3d_globe $HOME/local/
 
 exit $?
 
--- batlab/specfem3d/build.sh
+++ batlab/specfem3d/build.sh
@@ -3,9 +3,11 @@
 # Point to any additional installed libraries
 source ./build_common.sh
 
+# Ensure we have a common naming scheme
+mv SPECFEM3D*/ specfem3d > /dev/null 2>&1
+
 # Build SPECFEM3D
-cd SPECFEM3D* || cd specfem3d*
-./configure && make all
+cd specfem3d && ./configure && make all && cd .. && tar -czf results.tar.gz specfem3d $HOME/local/
 
 exit $?
 
