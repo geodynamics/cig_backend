bb6a06e0d7983303fc5ea0e2e4f08a582499ce36
Eric Heien <github@heien.org>
2013-07-26 19:28:50 -0500

    Ellipsis tests updated

--- batlab/ellipsis3d/build.sh
+++ batlab/ellipsis3d/build.sh
@@ -3,14 +3,14 @@
 # Point to any additional installed libraries
 source ./build_common.sh
 
-# Builds Ellipsis3D
-cd Ellipsis3* || cd ellipsis3*
+# Ensure we have a common naming scheme
+mv Ellipsis3*/ ellipsis3d > /dev/null 2>&1
 
 if [ "$1" == "repo" ]
 then
-    autoreconf -i && ./configure && make
+    cd ellipsis3d && autoreconf -i && ./configure && make && cd .. && tar -czf results.tar.gz ellipsis3d $HOME/local/
 else
-    ./configure && make
+    cd ellipsis3d && ./configure && make && cd .. && tar -czf results.tar.gz ellipsis3d $HOME/local/
 fi
 
 exit $?
