5b8b756c539f45ee70de38d7f4474f72e0d154eb
Eric Heien <github@heien.org>
2013-07-29 18:56:42 -0500

    Updated Mag tests

--- batlab/mag/build.sh
+++ batlab/mag/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Point to any additional installed libraries
+source ./build_common.sh
+
+# Ensure we have a common naming scheme
+mv MAG*/ mag > /dev/null 2>&1
+
 # Builds multiple variants of MAG
-cd MAG* || cd mag*
-cd src
+cd mag/src
+
+set -e
 
 GFORT_LOC=`which gfortran`
 ln -s $GFORT_LOC g77
@@ -10,11 +17,10 @@ export PATH=$PATH:.
 
 for PARAM in 32s1 32s4 32s6 96s6
 do
-	ln -sf param$PARAM.f param.f
-	make
-	mv magx ../magx$PARAM
-	make clean
+	ln -sf param$PARAM.f param.f && make && mv magx ../magx$PARAM && make clean
 done
+
+cd ../.. && tar -czf results.tar.gz mag $HOME/local/
 
 exit $?
 
---
+++ batlab/mag/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Point to any additional installed libraries
+source ./build_common.sh
+
+# Move to the CitcomS directory
+cd citcomcu
+
+# Move the Regional example
+if [ "$1" == "null" ]
+then
+    echo "Null test"
+else
+    echo "Unknown test: $1"
+    exit 1
+fi
+
+exit $?
+
