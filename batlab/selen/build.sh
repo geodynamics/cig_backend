ac93316294965584360376b860af9e24d1c2c204
Eric Heien <github@heien.org>
2013-07-29 18:45:23 -0500

    Updated SELEN tests

--- batlab/selen/build.sh
+++ batlab/selen/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# Point to any additional installed libraries
 source ./build_common.sh
 
 # To handle different names for distribution vs. repo
-mv SELEN* selen > /dev/null 2>&1
+mv SELEN*/ selen > /dev/null 2>&1
 
 # Build SELEN
-mv fast_config.dat selen/config.dat && cd selen && ./configure && make run
+mv fast_config.dat selen/config.dat && cd selen && ./configure && make run && cd .. && tar -czf results.tar.gz selen $HOME/local/
 
 exit $?
 
