933b297448c8f76b624e26fd3bcd9146b516fe53
Eric Heien <github@heien.org>
2013-08-30 16:04:42 -0500

    Fixed SPECFEM1D test, confirmed success

--- batlab/specfem1d/build.sh
+++ batlab/specfem1d/build.sh
@@ -7,7 +7,7 @@ source ./build_common.sh
 mv SPECFEM1D*/ specfem1d > /dev/null 2>&1
 
 # Build SPECFEM1D
-cd specfem1d && make && cd .. && tar -czf results.tar.gz specfem1d $CURDIR/local/
+cd specfem1d && make && cd .. && tar -czf results.tar.gz specfem1d local
 
 exit $?
 
