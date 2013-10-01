003e001e78a2c01ff54a4faad9e9d9d1a79fee97
Eric Heien <github@heien.org>
2013-10-01 16:34:30 -0700

    Updated SPECFEM2D tests

--- batlab/specfem2d/specfem2d_compare.py
+++ batlab/specfem2d/specfem2d_compare.py
@@ -7,42 +7,47 @@ import string
 
 file_descs = [
         # file name pattern, number of header lines, column layout, maximum error tolerances
-        ["S%04d.AA.BXX.semd", 0, [1, 1], [1e-6, 1e-6]],
-        ["S%04d.AA.BXZ.semd", 0, [1, 1], [1e-6, 1e-6]],
+        ["S{[step]:04d}.AA.BXX.semd", 0, [1, 1], [1e-6, 1e-6]],
+        ["S{[step]:04d}.AA.BXZ.semd", 0, [1, 1], [1e-6, 1e-6]],
         ]
+
 if len(sys.argv) < 4:
-    print "args: dir_1 dir_2 max_step step_size"
+    print("args: dir_1 dir_2 max_step")
     sys.exit(1)
 
-directories = [sys.argv[1], sys.argv[2]]
+dirs = [sys.argv[1], sys.argv[2]]
 max_step = int(sys.argv[3])
-step_size = int(sys.argv[4])
-compare_functions = [cig_compare.MaxMagDiffRenormed]
 
-print "FILE VAR ORIG TOL"
-total_tests = 0
-num_passed = 0
-for desc in file_descs[1:]:
-    for t in xrange(1,max_step,step_size):
-        result_data = [cig_compare.ResultData(desc[1], desc[2]) for d in directories]
-        file_name = desc[0] % (t,)
-        for i,dir_name in enumerate(directories): result_data[i].read_file_ascii(dir_name+"/"+file_name)
-        compare_result = cig_compare.compare(result_data, compare_functions)
-        # Ensure the results are within accepted tolerances
-        my_key = compare_result.keys()[0]
-        for i, tolerance in enumerate(desc[3]):
-            val = compare_result[my_key][i]
-            total_tests += 1
-            if val <= tolerance:
-                num_passed += 1
-                pass_fail = "PASS"
-            else:
-                all_passed = False
-                pass_fail = "FAIL"
-            print "%s %d %.10f %.10f %s" % (desc[0] % (t,), i, val, tolerance, pass_fail)
+comparisons = [
+    cig_compare.FileComparison(
+        directories=dirs,
+        file_name_template="S{[step]:04d}.AA.BXX.semd",
+        file_type="ascii",
+        num_header_lines=0,
+        column_layout=[1, 1],
+        compare_function=cig_compare.MaxMagDiffRenormed,
+        error_tolerances=[1e-6, 1e-6],
+        step_iter=xrange(1, max_step),
+        proc_iter=[0]),
+    cig_compare.FileComparison(
+        directories=dirs,
+        file_name_template="S{[step]:04d}.AA.BXZ.semd",
+        file_type="ascii",
+        num_header_lines=0,
+        column_layout=[1, 1],
+        compare_function=cig_compare.MaxMagDiffRenormed,
+        error_tolerances=[1e-6, 1e-6],
+        step_iter=xrange(1, max_step),
+        proc_iter=[0]),
+    ]
 
-print "END: Passed %d/%d tests" % (num_passed, total_tests)
-if num_passed == total_tests:
+passed_all = True
+for comp in comparisons:
+    comp.print_summary()
+    num_tests, num_failed = comp.test_statistics()
+    if num_failed > 0: passed_all = False
+
+if passed_all:
     sys.exit(0)
 else:
     sys.exit(1)
