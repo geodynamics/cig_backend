9b64662daf862b78dfd7fd91fabfc54285a1d13b
Eric Heien <github@heien.org>
2013-10-01 17:55:53 -0500

    Whitespace fix

--- batlab/specfem1d/specfem1d_compare.py
+++ batlab/specfem1d/specfem1d_compare.py
@@ -14,24 +14,26 @@ max_step = int(sys.argv[3])
 step_size = int(sys.argv[4])
 
 comparisons = [
-    cig_compare.FileComparison(directories=dirs,
-                   file_name_template="seismogram",
-                   file_type="ascii",
-                   num_header_lines=0,
-                   column_layout=[1, 1],
-                   compare_function=cig_compare.MaxMagDiffRenormed,
-                   error_tolerances=[0.02, 5e-4],
-                   step_iter=[0],
-                   proc_iter=[0]),
-    cig_compare.FileComparison(directories=dirs,
-                   file_name_template="snapshot_forward_normal{[step]:05d}",
-                   file_type="ascii",
-                   num_header_lines=0,
-                   column_layout=[1, 1],
-                   compare_function=cig_compare.MaxMagDiffRenormed,
-                   error_tolerances=[1e-6, 1e-6],
-                   step_iter=xrange(100, max_step, step_size),
-                   proc_iter=[0]),
+    cig_compare.FileComparison(
+        directories=dirs,
+        file_name_template="seismogram",
+        file_type="ascii",
+        num_header_lines=0,
+        column_layout=[1, 1],
+        compare_function=cig_compare.MaxMagDiffRenormed,
+        error_tolerances=[0.02, 5e-4],
+        step_iter=[0],
+        proc_iter=[0]),
+    cig_compare.FileComparison(
+        directories=dirs,
+        file_name_template="snapshot_forward_normal{[step]:05d}",
+        file_type="ascii",
+        num_header_lines=0,
+        column_layout=[1, 1],
+        compare_function=cig_compare.MaxMagDiffRenormed,
+        error_tolerances=[1e-6, 1e-6],
+        step_iter=xrange(100, max_step, step_size),
+        proc_iter=[0]),
     ]
 
 passed_all = True
